eded6551630197b2b37ad2d26afdbe3d9b680793
Ismael Haytam <anonismaa@gmail.com>
2017-06-09 23:14:07 +0200

    Transiciones

---
+++ 10-transiciones/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensaje: {
+            transicion: 'Transicion CSS con Vue.js',
+            animacion: 'Anomacion CSS con Vue.js',
+            animationCustom: 'Animaciones custom CSS con vue.js',
+            entreElementos: 'Transitiones entre elementos con Vue.js',
+        }
+    }
+});
